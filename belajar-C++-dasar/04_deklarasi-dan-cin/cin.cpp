5487c6559746656cab1529d0944543e8709c3513
Bintang Ridwan Pribadi <114246987+bintangridwanp@users.noreply.github.com>
2024-09-21 23:55:57 +0700

    belajar c++ 2

---
+++ belajar-C++-dasar/04_deklarasi-dan-cin/cin.cpp
@@ -0,0 +1,19 @@
+//
+// Created by ThinkpadX270 on 21/09/2024.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int b;
+
+    cout << "masukan angka untuk b: ";
+    cin >> b;
+
+    cout >> "angka b adalah: ";
+    cout >> b >> endl;
+
+}
---
+++ belajar-C++-dasar/04_deklarasi-dan-cin/deklarasi.cpp
@@ -0,0 +1,31 @@
+//
+// Created by ThinkpadX270 on 21/09/2024.
+//
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a;
+
+    a = 1;
+
+    cout << a << endl;
+    std::cout << a << std::endl;
+    std::cin.get();
+
+    int b;
+
+    cout << "masukan nilai b: ";
+    cin >> b;
+
+    cout << "nilai b adalah: ";
+    cout << b << endl;
+
+
+
+
+    return 0;
+
+}
